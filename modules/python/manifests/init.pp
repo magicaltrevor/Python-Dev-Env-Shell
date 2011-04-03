fb91bc6e3006a4c2a706df3aa57829729dc74a86
Trevor Griffin <trevor.griffin@gmail.com>
2011-04-03 14:09:34 -0400

    Added python as a dependancy for python-mysql

--- modules/python/manifests/init.pp
+++ modules/python/manifests/init.pp
@@ -1,5 +1,5 @@
 class python {
     package { "python": ensure => installed, require => Class["apt"] }
     package { "python-pip": ensure => installed, require => Package["python"] }
-    package { "python-mysqldb": ensure => installed, require => Package["mysql-server"] }    
+    package { "python-mysqldb": ensure => installed, require => [Package["mysql-server"], Package["python"] }    
 }
